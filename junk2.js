731175e09b1bddcdb43081607af00c2b3eb1d2b8
ray sonntag <raysonntag@yahoo.com>
2019-10-31 18:55:25 -0400

    1031 update

--- junk2.js
+++ junk2.js
@@ -1,2 +1,11 @@
 var a,b,c,d,e,i,n,o,r,s,t,aray=[],may=[],ray=[],say=[];
 
+ var inp1=document.getElementById("inp1");
+ var inp2=document.getElementById("inp2");
+ var bo1=document.getElementById("bo1");
+ var sum=document.getElementById("sum");
+ inp1.addEventListener("click",fun1);
+
+ function fun1() {
+     console.log("ray was here");
+ }
