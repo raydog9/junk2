e0d65877dc171d187e9d28e3a310dfed383e8391
ray sonntag <raysonntag@yahoo.com>
2019-12-13 19:57:12 -0500

    array elements

--- junk2.js
+++ junk2.js
@@ -7,17 +7,30 @@
  bo4=document.getElementById("bo4");
 
     bu1.addEventListener("click",fun1);
+    bu2.addEventListener("click",fun2);
 
 function fun1(list) {
  for (i=0;i<50;i++) {
      if (i%2==0 || i%9==0) {
          arry.push(i);
-        }
-         }
-         bo5.innerHTML=arry;
+         arry.splice(10,1);
 
+
+         bo5.innerHTML=arry;
+         }
+         
         }
-        bo6.innerHTML=arry;
+        
+        }
+        
+    function fun2() {
+        ray=arry.slice();
+        ray.reverse();
+        ray.splice(-2,1);
+ 
+
+         bo6.innerHTML=(ray);
+        }        
 
         
 
