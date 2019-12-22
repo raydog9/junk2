8069407b7aff03e630834919e883c4973c55791f
ray sonntag <raysonntag@yahoo.com>
2019-12-22 18:28:54 -0500

    update 12 22 19

--- junk2.js
+++ junk2.js
@@ -1,26 +1,37 @@
- var a,b,c,d,e,m,n,o,r,s,t,arry=[],may=[],ray=[],say=[];
+ var a,b,c,d,e,m,n,o,arry=[],may=[],ray=[],say=[];
 bo1=document.getElementById("bo1");
 bo2=document.getElementById("bo2");
+bo3=document.getElementById("bo3");
+bo4=document.getElementById("bo4");
 bo5=document.getElementById("bo5");
 bo6=document.getElementById("bo6");
 bo7=document.getElementById("bo7");
+bo8=document.getElementById("b08");
 
-inp1.addEventListener("input",fun1);
-inp2.addEventListener("input",fun1);
-bu2.addEventListener("click",fun2);
+bu1.addEventListener("click",fun1)
 
-function fun1(a,b) {
-    a=parseFloat(inp1.value) || 0;
-    b=parseFloat(inp2.value) || 0;
-    let sum=a+b;
-    bo5.innerHTML=a;
-    bo6.innerHTML=b;
-    bo7.innerHTML=sum;
-    return sum
+function fun1() {
+    const rand = {
+        ran1:Math.floor(Math.random() * 10),
+        ran2:Math.floor(Math.random() * 10),
+        ran3:Math.floor(Math.random() * 10),
+        ran4:Math.floor(Math.random () *10)
+        };
+    if ( rand.ran1 % 2 ==0) {
+        arry.push(rand.ran1);
+    }
+    else if ( rand.ran1 % 2 ==1 ) 
+        ray.push(rand.ran1)
+    
+
+
+
+
+    bo1.innerHTML=rand.ran1;
+    bo2.innerHTML=rand.ran2;
+    bo3.innerHTML=rand.ran3;
+    bo4.innerHTML=rand.ran4;
+    bo5.innerHTML=arry;
+    bo6.innerHTML=ray;
 }
 
-function fun2() {
-    bo5.innerHTML="";
-    inp1.value="";
-    inp2.value="";
-}
