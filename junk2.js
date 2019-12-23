3e95e2c86993605965c37943e88497d184aaa7c7
ray sonntag <raysonntag@yahoo.com>
2019-12-22 21:18:37 -0500

    update

--- junk2.js
+++ junk2.js
@@ -1,4 +1,4 @@
- var a,b,c,d,e,m,n,o,arry=[],may=[],ray=[],say=[];
+ var a,b,c,d,e,m,n,o,x,arry=[],may=[],ray=[],say=[];
 bo1=document.getElementById("bo1");
 bo2=document.getElementById("bo2");
 bo3=document.getElementById("bo3");
@@ -6,13 +6,15 @@ bo4=document.getElementById("bo4");
 bo5=document.getElementById("bo5");
 bo6=document.getElementById("bo6");
 bo7=document.getElementById("bo7");
-bo8=document.getElementById("b08");
+bo8=document.getElementById("bo8");
 
 bu1.addEventListener("click",fun1);
 bu2.addEventListener("click",fun2);
 
 function fun1() {
-    const rand = {
+    x++;
+    i=parseFloat(x);
+      const rand = {
         ran1:Math.floor(Math.random() * 10),
         ran2:Math.floor(Math.random() * 10),
         ran3:Math.floor(Math.random() * 10),
@@ -20,19 +22,37 @@ function fun1() {
         };
     if ( rand.ran1 % 2 ==0) {
         arry.push(rand.ran1);
+        bo5.innerHTML=rand.ran1;
+
+        
     }
     else if ( rand.ran1 % 2 ==1 ) {
         ray.push(rand.ran1)
+        bo6.innerHTML=rand.ran1;
+
     }
-    bo1.innerHTML=rand.ran1;
-    bo2.innerHTML=rand.ran2;
-    bo3.innerHTML=rand.ran3;
-    bo4.innerHTML=rand.ran4;
-    bo5.innerHTML=arry;
-    bo6.innerHTML=ray;
+
+    if( rand.ran2 % 2 == 0) {
+        may.push(rand.ran2);
+        bo7.innerHTML=rand.ran2;
+
+    }
+    else if (rand.ran2 % 2 == 1) {
+        say.push(rand.ran2);
+        bo8.innerHTML=rand.ran2;
+
+    }
+     bo9.innerHTML=arry;
+    b10.innerHTML=ray;
+    b11.innerHTML=may;
+    b12.innerHTML=say;
+    b13.innerHTML=i;
 }
 function fun2() {
     bo5.innerHTML="";
     bo6.innerHTML="";
+    bo7.innerHTML="";
+    bo8.innerHTML="";
+
 }
 
