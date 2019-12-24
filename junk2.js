c569893261ae7fa81b2dc635eaef6fc200eff1a2
ray sonntag <raysonntag@yahoo.com>
2019-12-24 03:44:51 -0500

    updated

--- junk2.js
+++ junk2.js
@@ -2,7 +2,7 @@
 
         
 
-   var count = 0;
+  var count = 0;
   var button = document.getElementById("bu1");
 
   button.onclick = function(){
@@ -16,8 +16,14 @@
   div5.onclick = function() {
       counts++;
       bo5.innerHTML=counts;
-
-  }
+    }
+    var div9=document.getElementById("bo9");
+        cccount=0;
+    
+    div9.onclick = function() {
+        cccount=count+counts;
+        bo9.innerHTML=cccount;
+    }
 
 
 
